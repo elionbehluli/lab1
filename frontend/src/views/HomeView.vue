83eec8877837bd029f8db75ce7e5858a5300177f
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2024-03-23 00:55:33 +0100

    feat: removed unnessecary code, added tailwind and postcss

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <WelcomeHome />
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import WelcomeHome from '../components/WelcomeHome.vue'
+</script>
