97d3dcb813cb9174eb94a9bd52cf9782867e8ad1
elionbehluli <elionbehluli7@gmail.com>
2024-04-08 01:19:53 +0200

    feat: finalized implement user login on backend

--- backend/app/Http/Controllers/Auth/UserController.php
+++ backend/app/Http/Controllers/Auth/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 
 class UserController extends Controller
@@ -15,9 +18,26 @@ class UserController extends Controller
         return 'registertest';
     }
 
-    public function login()
+    public function login(LoginRequest $request)
     {
-        return 'logintest';
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+
+        // Retrieve the authenticated user
+        $user = auth()->user();
+
+        // Return the token along with user information
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
     }
 
     public function logout()
