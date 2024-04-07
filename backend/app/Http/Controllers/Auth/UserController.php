c805307c1a9f439dea3f512dae7e95d49d5116b1
elionbehluli <elionbehluli7@gmail.com>
2024-04-08 01:04:27 +0200

    feat: finalized implement user register on backend

--- backend/app/Http/Controllers/Auth/UserController.php
+++ backend/app/Http/Controllers/Auth/UserController.php
@@ -3,16 +3,44 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserRegisterRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 
 class UserController extends Controller
 {
    
-    public function register()
+    public function register(UserRegisterRequest $request)
     {
-        return 'registertest';
+        $request->validated();
+
+        $userData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+
+        // Check if the request is coming from an authenticated admin
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            // If admin, assign 'employee' role
+            $userData['role'] = 'employee';
+        } else {
+            // If not admin, assign 'customer' role by default
+            $userData['role'] = 'customer';
+        }
+
+        $user = User::create($userData);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+
     }
 
     public function login()
