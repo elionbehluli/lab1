260dba0e674fb4c204108f5603b0d30718338fb8
elionbehluli <elionbehluli7@gmail.com>
2024-04-08 19:13:41 +0200

    feat: finalized create car brand model and migration on backend

---
+++ backend/app/Models/Brand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'country_of_origin',
+        'logo_url',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
