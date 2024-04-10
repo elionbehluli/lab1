6e9c2bf0f53c721c24587f61a01544c1a98a61b7
LindritRamusa <lindrit.ramusa5@gmail.com>
2024-04-10 16:37:50 +0200

    feat: finalized subtask 4&5 of navbar task

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NavbarLayout from '../layouts/NavbarLayout.vue'
+import AboutView from '../views/AboutView.vue'
+import ServicesView from '@/views/ServicesView.vue'
+import CarsView from '@/views/CarsView.vue'
+import ContactView from '@/views/ContactView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +15,20 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+
+    {
+      path: '/',
+      component: NavbarLayout,
+      children: [
+        { path: '', redirect: '/about' },
+        { path: 'about', component: AboutView },
+        { path: 'services', component: ServicesView },
+        { path: 'cars', component: CarsView },
+        { path: 'contact', component: ContactView },
+        { path: 'login', component: LoginView },
+        { path: 'register', component: RegisterView }
+      ]
     }
   ]
 })
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-3xl font-bold mb-4">About</h1>
+    <p class="text-lg text-gray-600">We are here to improve. Let's do our best!</p>
+    <button
+      class="mt-8 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+    >
+      Get Started
+    </button>
+  </div>
+</template>
