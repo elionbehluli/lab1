e148419830780763238e24aeaa7aad7fed36bd19
LindritRamusa <lindrit.ramusa5@gmail.com>
2024-04-12 17:17:13 +0200

    feat: added on hover highlight for buttons

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,19 +5,19 @@
         <div class="flex space-x-4">
           <router-link to="/" class="text-white text-xl font-bold">Your Logo</router-link>
 
-          <router-link to="/" class="text-white hover:text-gray-300">Home</router-link>
-          <router-link to="/about" class="text-white hover:text-gray-300">About</router-link>
-          <router-link to="/services" class="text-white hover:text-gray-300">Services</router-link>
-          <router-link to="/cars" class="text-white hover:text-gray-300">Cars</router-link>
-          <router-link to="/contact" class="text-white hover:text-gray-300">Contact</router-link>
+          <router-link to="/" class="page-link">Home</router-link>
+          <router-link to="/about" class="page-link">About</router-link>
+          <router-link to="/services" class="page-link">Services</router-link>
+          <router-link to="/cars" class="page-link">Cars</router-link>
+          <router-link to="/contact" class="page-link">Contact</router-link>
         </div>
 
         <div class="flex space-x-4">
           <router-link to="/login" class="text-white hover:text-gray-300">
-            <button class="text-white hover:text-gray-300">Login</button>
+            <button class="button">Login</button>
           </router-link>
           <router-link to="/register" class="text-white hover:text-gray-300">
-            <button class="text-white hover:text-gray-300">Register</button>
+            <button class="button">Register</button>
           </router-link>
         </div>
       </div>
