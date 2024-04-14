411f1c2cbf8f62dcbdae5645d643f6a58712f132
elionbehluli <elionbehluli7@gmail.com>
2024-04-14 22:09:50 +0200

    feat: finalized user forgot password on backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserController;
+use App\Models\User;
 
 Route::group([
 
@@ -14,5 +15,6 @@ Route::group([
     Route::post('/login', [UserController::class, 'login']);
     Route::post('/refreshToken', [UserController::class, 'refreshToken']);
     Route::post('/logout', [UserController::class, 'logout']);
-    Route::post('/forgotPassword', [UserController::class, 'forgotPassword']);
+    Route::post('/password/forgot', [UserController::class, 'forgotPassword']);
+    Route::post('/password/reset', [UserController::class, 'resetPassword'])->name('password.reset');
 });
