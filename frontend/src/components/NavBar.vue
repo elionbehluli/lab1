78355eee0f1ba80081c8510f3b270dba5afb3e1f
LindritRamusa <lindrit.ramusa5@gmail.com>
2024-04-18 17:15:58 +0200

    fix: added active link highlighting in navbar and refactor button styles and hover effects

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,22 +5,67 @@
         <div class="flex space-x-4">
           <router-link to="/" class="text-white text-xl font-bold">Your Logo</router-link>
 
-          <router-link to="/" class="page-link">Home</router-link>
-          <router-link to="/about" class="page-link">About</router-link>
-          <router-link to="/services" class="page-link">Services</router-link>
-          <router-link to="/cars" class="page-link">Cars</router-link>
-          <router-link to="/contact" class="page-link">Contact</router-link>
+          <router-link to="/" class="page-link" :class="{ 'active-link': $route.path === '/' }"
+            >Home</router-link
+          >
+          <router-link
+            to="/about"
+            class="page-link"
+            :class="{ 'active-link': $route.path === '/about' }"
+            >About</router-link
+          >
+          <router-link
+            to="/services"
+            class="page-link"
+            :class="{ 'active-link': $route.path === '/services' }"
+            >Services</router-link
+          >
+          <router-link
+            to="/cars"
+            class="page-link"
+            :class="{ 'active-link': $route.path === '/cars' }"
+            >Cars</router-link
+          >
+          <router-link
+            to="/contact"
+            class="page-link"
+            :class="{ 'active-link': $route.path === '/contact' }"
+            >Contact</router-link
+          >
         </div>
 
         <div class="flex space-x-4">
           <router-link to="/login" class="text-white hover:text-gray-300">
-            <button class="button">Login</button>
+            <button
+              class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+            >
+              Login
+            </button>
           </router-link>
           <router-link to="/register" class="text-white hover:text-gray-300">
-            <button class="button">Register</button>
+            <button class="py-1 px-2">Register</button>
           </router-link>
         </div>
       </div>
     </div>
   </nav>
 </template>
+<style>
+.page-link,
+.button {
+  color: white;
+  transition: all 0.3s ease;
+}
+
+.page-link:hover {
+  background-color: rgb(59 130 246);
+  border-radius: 8px;
+  padding: 0.1em 0.5em;
+}
+
+.active-link {
+  background-color: rgb(59 130 246);
+  border-radius: 8px;
+  padding: 0.1em 0.5em;
+}
+</style>
